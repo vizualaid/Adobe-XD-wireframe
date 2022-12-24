aa038736dcfcbca9eaf8df28520751474992c23b
vizualaid <isha.12011399@lpu.in>
2022-12-24 21:52:06 +0530

    updates

---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#E0E0E0","shell":"iPhone","showControlPanel":true,"iOSGuide":true,"alwaysShowLinks":false,"hoverShowLinks":true,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":true,"scrollRemarks":true,"isShowAllPageNode":true,"isShowPreviewTools":true,"showRemarkFlag":true}
